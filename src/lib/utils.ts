800ec40056516581b819827f3804feca117acb10
VictorSantuccii <victorfregne@gmail.com>
2025-09-03 15:13:07 -0300

    feat: new settings for utilities

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,27 @@
-import { clsx, type ClassValue } from "clsx"
+import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function formatCurrency(value: number): string {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  }).format(value)
+}
+
+export function formatDate(date: Date): string {
+  return new Intl.DateTimeFormat('pt-BR', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit'
+  }).format(date)
+}
+
+export function generateQRCodeUrl(restaurantId: string): string {
+  return `${typeof window !== 'undefined' ? window.location.origin : ''}/menu/qr-scanner?restaurant=${restaurantId}`
 }
